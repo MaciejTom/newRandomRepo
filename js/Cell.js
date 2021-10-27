c70bc552f6a5c13cc04e12edf5fe0cf3dbffa98a
Maciej Tomaszewicz <1994tomaszewicz@gmail.com>
2021-10-27 12:48:43 +0200

    basic files  added

---
+++ js/Cell.js
@@ -0,0 +1,27 @@
+import { UI } from "./UI.js"
+
+export class Cell extends UI {
+    constructor(x, y) {
+        super();
+        this.x = x;
+        this.y = y;
+        this.value = 0;
+        this.isMine = false;
+        this.isReavel = false;
+        this.isFlagged = false;
+        this.selector = `[data-x="${this.x}"][data-y="${this.y}"]`;
+        this.element = null;
+    }
+    createElement() {
+        const element = `<div class="cell border border--concave" data-cell data-x="${this.x}" data-y="${this.y}"></div>`;
+        return element;
+    }
+    revealCell() {
+      
+        this.isReveal = true;
+        console.log(this)
+       
+        console.log(this)
+      
+    }
+    }
--- js/Game.js
+++ js/Game.js
@@ -1,12 +1,27 @@
 class Game {
-
-
-    initialazeGame() {
-         
+    #config = {
+        easy = {
+            rows: 8,
+            cols: 8,
+            mines: 10
+        },
+        medium = {
+            rows: 16,
+            cols: 16,
+            mines: 40
+        },
+        expert = {
+            rows: 16,
+            cols: 30,
+            mines: 99
+        }
     }
+    #numberOfRows = null;
+    #numberOfCols = null;
+    #numberOfMines = null;
 }
 
 window.onload = function() {
-    const game = new Game();
-    game.initialazeGame()
+    const game = new Game(); 
+    game.initializeGame();
 }
