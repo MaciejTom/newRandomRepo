2205e781f2f4f303c549fd87bc817455313a2dba
Maciej Tomaszewicz <1994tomaszewicz@gmail.com>
2021-10-27 14:03:15 +0200

    Cells pushed to array

--- js/Cell.js
+++ js/Cell.js
@@ -1,27 +1,12 @@
-import { UI } from "./UI.js"
-
-export class Cell extends UI {
+export class Cell {
     constructor(x, y) {
-        super();
         this.x = x;
         this.y = y;
         this.value = 0;
         this.isMine = false;
-        this.isReavel = false;
+        this.isReveal = false;
         this.isFlagged = false;
         this.selector = `[data-x="${this.x}"][data-y="${this.y}"]`;
         this.element = null;
     }
-    createElement() {
-        const element = `<div class="cell border border--concave" data-cell data-x="${this.x}" data-y="${this.y}"></div>`;
-        return element;
-    }
-    revealCell() {
-      
-        this.isReveal = true;
-        console.log(this)
-       
-        console.log(this)
-      
-    }
-    }
+}
